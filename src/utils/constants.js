1179390d5e87c0ae5364d0e3a669777a696a386c
Prince Kumar <princeuiet123@gmail.com>
2026-02-26 03:34:34 +0530

    frontend deployed on vercel

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,1 +1,1 @@
-export const BASE_URL = "/api"
+export const BASE_URL = "https://exuverse-assignment-backend-2.onrender.com"
--- vite.config.js
+++ vite.config.js
@@ -3,14 +3,4 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  server: {
-    proxy: {
-      '/api': {
-        target: 'https://exuverse-assignment-backend-2.onrender.com',
-        changeOrigin: true,
-        secure: false,
-        rewrite: (path) => path.replace(/^\/api/, '') 
-      }
-    }
-  }
 })
